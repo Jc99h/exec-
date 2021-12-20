019bb6ee30174fc0a185f529c9d9d9c9bc4bcc3d
CamiloHurtado99 <95389919+Jc99h@users.noreply.github.com>
2021-12-20 22:29:35 +0000

    Finalizado Shell basico

--- leercadena_main.c
+++ leercadena_main.c
@@ -13,31 +13,31 @@
 #include <unistd.h>
 #include "LeerCadena.h"
 
-int main(int argc, char *argv[]) {
-  char comando[BUFSIZ];
-  char **vector;
-  int i;
-
-  while (1) {
-    pid_t pid;
-    printf("> ");
-    leer_de_teclado(BUFSIZ,comando);
-    if (strcmp("exit",comando) == 0) {
-      break;
-    }
-    pid = fork();
-    if (pid < 0) {
-      perror("No se pudo crear un proceso\n");
-      exit(1);
-    } else if (pid == 0) {
-      vector = de_cadena_a_vector(comando);
-      execvp(vector[0],vector);
-      perror("Return from execlp() not expected");
-      exit(EXIT_FAILURE);
-    } else {
-      wait(NULL);
+int main(int argc, char *argv[]) 
+{
+  int cual=0;
+  char *str[argc];
+  
+  for(cual; cual<argc; cual++)
+  {
+    str[cual]=argv[cual+1];
+    if(cual+1==argc)
+    {
+      str[cual+1]=NULL;
     }
   }
 
+  pid_t pid;
+  pid = fork();
+  if (pid < 0) {
+    perror("No se pudo crear un proceso\n");
+    exit(1);
+  } else if (pid == 0) {
+    execvp(str[0], str);
+    perror("Return from execlp() not expected");
+    exit(EXIT_FAILURE);
+  } else {
+    wait(NULL);
+  }
   return 0;
 }
